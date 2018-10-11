1fe2fa40024ee40908b80b6ef9b965500ab03e02
arseny <asvedr@gmail.com>
2018-10-11 22:37:12 +0300

    refactoring

--- logic/logic.py
+++ logic/logic.py
@@ -4,55 +4,55 @@ from logic.field import Field, Cell
 from logic.pers import Pers
 
 class Logic(object):
-	
-	__slots__ = ('ghosts', 'pacman', 'field',
-	             'user_vector', '_stop_flag',
-				 'previous_time', 'diff_time')
-	user_vector_none = (0, 0)
-	
-	@classmethod
-	def load_file(cls, path):
-		return cls(Field(path))
-	
-	def __init__(self, field):
-		self.field = field
-		self.pacman = None
-		self.ghosts = []
-		self.user_vector = self.user_vector_none
-		self._stop_flag = False
-		
-		perses = {Cell.Pacman: Pers.pacman,
-		          Cell.RGhost: Pers.red,
-				  Cell.BGhost: Pers.blue,
-				  Cell.YGhost: Pers.yello,
-				  Cell.PGhost: Pers.pink}
-		
-		for y in range(field.height):
-			for x in range(field.width):
-				cell = field.data[y][x]
-				if cell in perses:
-					pers = perses[cell](x, y)
-					if pers.is_pacman:
-						self.pacman = pers
-					else:
-						self.ghosts.append(pers)
-					field.data[y][x] = Cell.Empty
-					
-	def stop(self):
-		self._stop_flag = True
-	
-	def run(self):
-		threading.Thread(target=self._mainloop).start()
+    
+    __slots__ = ('ghosts', 'pacman', 'field',
+                 'user_vector', '_stop_flag',
+                 'previous_time', 'diff_time')
+    user_vector_none = (0, 0)
+    
+    @classmethod
+    def load_file(cls, path):
+        return cls(Field(path))
+    
+    def __init__(self, field):
+        self.field = field
+        self.pacman = None
+        self.ghosts = []
+        self.user_vector = self.user_vector_none
+        self._stop_flag = False
+        
+        perses = {Cell.Pacman: Pers.pacman,
+                  Cell.RGhost: Pers.red,
+                  Cell.BGhost: Pers.blue,
+                  Cell.YGhost: Pers.yello,
+                  Cell.PGhost: Pers.pink}
+        
+        for y in range(field.height):
+            for x in range(field.width):
+                cell = field.data[y][x]
+                if cell in perses:
+                    pers = perses[cell](x, y)
+                    if pers.is_pacman:
+                        self.pacman = pers
+                    else:
+                        self.ghosts.append(pers)
+                    field.data[y][x] = Cell.Empty
+                    
+    def stop(self):
+        self._stop_flag = True
+    
+    def run(self):
+        threading.Thread(target=self._mainloop).start()
 
-	def tick(self):
-		now = time.time()
-		self.diff_time = now - self.previous_time
-		self.pacman.move(self)
-		self.user_vector = self.user_vector_none
-		for ghost in self.ghosts:
-			ghost.move(self)
-		time.sleep(1)
+    def tick(self):
+        now = time.time()
+        self.diff_time = now - self.previous_time
+        self.pacman.move(self)
+        self.user_vector = self.user_vector_none
+        for ghost in self.ghosts:
+            ghost.move(self)
+        time.sleep(1)
 
-	def _mainloop(self):
-		while not self._stop_flag:
-			self.tick()
+    def _mainloop(self):
+        while not self._stop_flag:
+            self.tick()
--- logic/pers.py
+++ logic/pers.py
@@ -7,68 +7,68 @@ PINK_SPEED = 0.5
 YELLO_SPEEd = 0.5
 
 class Pers(object):
-	
-	__slots__ = 'x', 'y', 'color', 'algorithm', 'speed', 'vector', 'is_pacman'
-	
-	@classmethod
-	def pacman(cls, x, y):
-		pacman = cls(x, y, pacman_algorithm, None)
-		pacman.vector = (0, 0)
-		pacman.is_pacman = True
-		return pacman
+    
+    __slots__ = 'x', 'y', 'color', 'algorithm', 'speed', 'vector', 'is_pacman'
+    
+    @classmethod
+    def pacman(cls, x, y):
+        pacman = cls(x, y, pacman_algorithm, None)
+        pacman.vector = (0, 0)
+        pacman.is_pacman = True
+        return pacman
 
-	@classmethod
-	def red(cls, x, y):
-		return cls(x, y, red_algorithm, 'red')
+    @classmethod
+    def red(cls, x, y):
+        return cls(x, y, red_algorithm, 'red')
 
-	@classmethod
-	def yello(cls, x, y):
-		return cls(x, y, yello_algorithm, 'yello')
+    @classmethod
+    def yello(cls, x, y):
+        return cls(x, y, yello_algorithm, 'yello')
 
-	@classmethod
-	def pink(cls, x, y):
-		return cls(x, y, pink_algorithm, 'pink')
+    @classmethod
+    def pink(cls, x, y):
+        return cls(x, y, pink_algorithm, 'pink')
 
-	@classmethod
-	def blue(cls, x, y):
-		return cls(x, y, blue_algorithm, 'blue')
-		
-	def __init__(self, x, y, algorithm, color):
-		self.x = x
-		self.y = y
-		self.algorithm = algorithm
-		self.color = color
-		self.vector = None
-		self.is_pacman = False
-	
-	def move(self, logic):
-		self.algorithm(self, logic)	
+    @classmethod
+    def blue(cls, x, y):
+        return cls(x, y, blue_algorithm, 'blue')
+        
+    def __init__(self, x, y, algorithm, color):
+        self.x = x
+        self.y = y
+        self.algorithm = algorithm
+        self.color = color
+        self.vector = None
+        self.is_pacman = False
+    
+    def move(self, logic):
+        self.algorithm(self, logic)    
 
-	def regular_move(self, logic, nx, ny):
-		field = logic.field
-		if not (0 <= nx < field.width or 0 <= ny < field.height):
-			raise Exception('out of map')
-		self.x = nx
-		self.y = ny
+    def regular_move(self, logic, nx, ny):
+        field = logic.field
+        if not (0 <= nx < field.width or 0 <= ny < field.height):
+            raise Exception('out of map')
+        self.x = nx
+        self.y = ny
 
 
 def pacman_algorithm(pers, logic):
-	has_turn = False
-	if logic.user_vector != EMPTY_VECTOR:
-		has_turn = True
-		pers.vector = logic.user_vector
-	x = pers.x + pers.vector[0] * PACMAN_SPEED * logic.diff_time
-	y = pers.y + pers.vector[1] * PACMAN_SPEED * logic.diff_time
-	pers.regular_move(logic, x, y)
+    has_turn = False
+    if logic.user_vector != EMPTY_VECTOR:
+        has_turn = True
+        pers.vector = logic.user_vector
+    x = pers.x + pers.vector[0] * PACMAN_SPEED * logic.diff_time
+    y = pers.y + pers.vector[1] * PACMAN_SPEED * logic.diff_time
+    pers.regular_move(logic, x, y)
 
 
 def red_algorithm(pers, logic):
-	# x = logic.pacman.x
-	# y = logic.pacman.y
-	x = pers.x
-	y = pers.y
-	pers.regular_move(logic, x, y)
-	
+    # x = logic.pacman.x
+    # y = logic.pacman.y
+    x = pers.x
+    y = pers.y
+    pers.regular_move(logic, x, y)
+    
 
 blue_algorithm = red_algorithm
 pink_algorithm = red_algorithm
