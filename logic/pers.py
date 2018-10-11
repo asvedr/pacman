6569601514c5f123b55243e6edcb259ba03c929b
arseny <asvedr@gmail.com>
2018-10-11 22:35:08 +0300

    pers bones

--- logic/pers.py
+++ logic/pers.py
@@ -8,34 +8,38 @@ YELLO_SPEEd = 0.5
 
 class Pers(object):
 	
-	__slots__ = 'x', 'y', 'color', 'algorithm', 'speed'
+	__slots__ = 'x', 'y', 'color', 'algorithm', 'speed', 'vector', 'is_pacman'
 	
 	@classmethod
 	def pacman(cls, x, y):
 		pacman = cls(x, y, pacman_algorithm, None)
 		pacman.vector = (0, 0)
+		pacman.is_pacman = True
+		return pacman
 
 	@classmethod
 	def red(cls, x, y):
-		cls(x, y, red_algorithm, 'red')
+		return cls(x, y, red_algorithm, 'red')
 
 	@classmethod
 	def yello(cls, x, y):
-		cls(x, y, yello_algorithm, 'yello')
+		return cls(x, y, yello_algorithm, 'yello')
 
 	@classmethod
 	def pink(cls, x, y):
-		cls(x, y, pink_algorithm, 'pink')
+		return cls(x, y, pink_algorithm, 'pink')
 
 	@classmethod
 	def blue(cls, x, y):
-		cls(x, y, blue_algorithm, 'blue')
+		return cls(x, y, blue_algorithm, 'blue')
 		
 	def __init__(self, x, y, algorithm, color):
 		self.x = x
 		self.y = y
 		self.algorithm = algorithm
 		self.color = color
+		self.vector = None
+		self.is_pacman = False
 	
 	def move(self, logic):
 		self.algorithm(self, logic)	
@@ -47,6 +51,7 @@ class Pers(object):
 		self.x = nx
 		self.y = ny
 
+
 def pacman_algorithm(pers, logic):
 	has_turn = False
 	if logic.user_vector != EMPTY_VECTOR:
@@ -55,3 +60,16 @@ def pacman_algorithm(pers, logic):
 	x = pers.x + pers.vector[0] * PACMAN_SPEED * logic.diff_time
 	y = pers.y + pers.vector[1] * PACMAN_SPEED * logic.diff_time
 	pers.regular_move(logic, x, y)
+
+
+def red_algorithm(pers, logic):
+	# x = logic.pacman.x
+	# y = logic.pacman.y
+	x = pers.x
+	y = pers.y
+	pers.regular_move(logic, x, y)
+	
+
+blue_algorithm = red_algorithm
+pink_algorithm = red_algorithm
+yello_algorithm = red_algorithm
